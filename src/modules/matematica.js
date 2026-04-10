6c2ee598035b24e49cb211abc8161e50239a2dc1
Android Studio <devandroid@ort>
2026-04-10 12:09:01 -0300

    error en el ultimo ejercicio

--- src/modules/matematica.js
+++ src/modules/matematica.js
@@ -1,16 +1,17 @@
 /* Este es el módulo "matematicas" */
 const PI = 3.14;
 function sumar(x, y) {
-return x+y
+return parseInt(x) + parseInt(y)
+
 }
 const multiplicar = (a, b) => {
-return a*b
+return parseInt(a)*parseInt(b)
 };
 function restar(x, y) {
-return x-y
+return parseInt(x)-parseInt(y)
 }
 const dividir = (a, b) => {
-return a/b
+return parseInt(a)/parseInt(b)
 };
 // Exporto todo lo que yo quiero exponer del módulo hacia el exterior.
 export {PI, sumar, multiplicar, dividir, restar};
--- src/test.js
+++ src/test.js
@@ -13,18 +13,43 @@ app.get('/', (req, res) => {
 })
 
 app.get('/saludar/:nombre', (req, res) => {
-    res.send('Hola: ' + req.params.nombre)
+    res.send('Hola ' + req.params.nombre + ', espero que tengas un hermoso dia')
 })
 
 app.get('/validarfecha/:ano/:mes/:dia', (req, res) => {
     if (!isNaN(req.params.ano) && !isNaN(req.params.mes) && !isNaN(req.params.dia)) {
 
-        if (Date.parse(`"${req.params.ano} - ${req.params.mes} - ${req.params.dia}"`)) {
+        if (Date.parse(`"${req.params.ano}-${req.params.mes}-${req.params.dia}"`)) {
             res.send(200)
         }
     }
     else res.send(400)  
 
+})
+
+app.get('/matematica/sumar', (req, res) =>{
+    
+    res.send(`Status 200 (OK) -- ${req.query.n1} + ${req.query.n2} = ${sumar(req.query.n1, req.query.n2)}`)
+})
+
+app.get('/matematica/multiplicar', (req, res) =>{
+    
+    res.send(`Status 200 (OK) -- ${req.query.n1} * ${req.query.n2} = ${multiplicar(req.query.n1, req.query.n2)}`)
+})
+
+app.get('/matematica/restar', (req, res) =>{
+    
+    res.send(`Status 200 (OK) -- ${req.query.n1} - ${req.query.n2} = ${restar(req.query.n1, req.query.n2)}`)
+})
+
+app.get('/matematica/dividir', (req, res) =>{
+    
+    res.send(`Status 200 (OK) -- ${req.query.n1} / ${req.query.n2} = ${dividir(req.query.n1, req.query.n2)}`)
+})
+
+app.get('/omdb/searchbypage', (req, res) =>{
+    let objeto = OMDBSearchByPage(req.query.title, req.query.page)
+    res.send(`Status 200 (OK) -- ${objeto}`) /*aca hay un error, como mostramos?*/
 })
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
