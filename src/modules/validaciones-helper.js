c7df622c55553d5290d62eb1ac6c25c18ba92019
Agos <agos@MacBook-Air-de-Agostina.local>
2026-04-19 19:46:26 -0300

    parte 1 de validaciones terminada

--- src/modules/validaciones-helper.js
+++ src/modules/validaciones-helper.js
@@ -1,16 +1,16 @@
 
-function getIntegerOrDefault(value, defaultValue) {
+getIntegerOrDefault = (value, defaultValue) => {
     let valorParseado = parseInt(value)
     if (isNaN(valorParseado)) {
         return defaultValue;
     } else return valorParseado
 }
-function getStringOrDefault(value, defaultValue) {
+ getStringOrDefault = (value, defaultValue) => { 
     if (value === null || value === undefined) {
         return defaultValue
     } else return value.toString()
 }
-function getDateOrDefault(value, defaultValue) {
+getDateOrDefault = (value, defaultValue) => {
     let fecha = new Date(value);
 
     if (isNaN(fecha.getTime())) {
@@ -19,13 +19,13 @@ function getDateOrDefault(value, defaultValue) {
         return fecha;
     }
 }
-function getBoolOrDefault(value, defaultValue) {
+getBoolOrDefault = (value, defaultValue) => {
     valor = value.toString().toLowerCase()
     if (valor === "true" || valor === "false") {
         return value
     } else return defaultValue
 }
-function isEmail(value) {
+isEmail = (value) =>{
     if (typeof value !== "string") return false
     else {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
